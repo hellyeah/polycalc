3afdcd0654b32cb7ad787ed845e45d8a6c9a0c11
Dave Font <gailees@umich.edu>
2021-10-08 23:51:56 -0400

    modifying some of the views and added picker view

--- polycalc/ContentView.swift
+++ polycalc/ContentView.swift
@@ -11,8 +11,25 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "swift")
+                .resizable()
+                .padding([.leading, .bottom, .trailing])
+                .frame(width: 100.0, height: 100.0)
+                .background(Color.orange)
             Text("Polycalc!")
+                .padding()
+            Text("Attacker!")
+            Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Unit")) {
+                Text("Warrior").tag(1)
+                Text("Rider").tag(2)
+            }
+            Text("Defender!")
+            Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Unit")) {
+                Text("Warrior").tag(1)
+                Text("Rider").tag(2)
+            }
         }
+        .padding()
+        
 
     }
 }
