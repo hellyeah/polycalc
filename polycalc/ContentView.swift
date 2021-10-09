b9cf695576dae1340d255debe98be8b394159d03
Dave Font <gailees@umich.edu>
2021-10-08 23:44:18 -0400

    basic swiftui stack

---
+++ polycalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  polycalc
+//
+//  Created by hellyeah on 10/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+            Text("Polycalc!")
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ polycalc/polycalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  polycalcApp.swift
+//  polycalc
+//
+//  Created by hellyeah on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct polycalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
